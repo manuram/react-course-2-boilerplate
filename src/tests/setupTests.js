ac67d388ca39460e13ef2919d2a57e38cdc96594
Manoj Ramachandran <ramachandranm@email.chop.edu>
2018-02-22 20:28:03 -0500

    Init commit

---
+++ src/tests/setupTests.js
@@ -0,0 +1,17 @@
+//import Enzyme from 'enzyme';
+//import Adapter from 'enzyme-adapter-react-16';
+//import DotEnv from 'dotenv';
+require('dotenv').config({ path: '.env.test' });
+
+
+
+//equire('dotenv').config({ path: '.env.test' });
+
+/*Enzyme.configure({
+    adapter: new Adapter()
+});*/
+/*DotEnv.config({
+    path: '.env.test'
+});*/
+
+//require('dotenv').config({ path: '.env.test' });
